152a19d324cfb7a47dde54446427d423b878386b
pandazz77 <kalin1538inn@gmail.com>
2026-02-10 21:37:43 +0300

    ClickControl: fix emitting while drag

--- src/Latte/Graphics/View/Controls/ClickControl.cpp
+++ src/Latte/Graphics/View/Controls/ClickControl.cpp
@@ -7,6 +7,13 @@
 bool ClickControl::onViewportEvent(QEvent *event){
     bool isPress = event->type() == QEvent::MouseButtonPress;
     bool isRelease = event->type() == QEvent::MouseButtonRelease;
+    bool isMoving = event->type() == QEvent::MouseMove;
+
+    // drop click event if it is drag
+    if(isMoving){
+        pressPos = QPointF();
+        return false;
+    }
 
     if(isPress || isRelease){
         QMouseEvent *mouseEvent = dynamic_cast<QMouseEvent*>(event);
